2a9fa98c3798e9e4d02d3a121af53015c5ceaf1b
HamzaAlee3011 <ameer.hamza.alee3011@gmail.com>
2024-10-21 21:03:54 +0500

    Update AASHTO_FPD_equation_solver.py

--- views/AASHTO_FPD_equation_solver.py
+++ views/AASHTO_FPD_equation_solver.py
@@ -13,14 +13,22 @@ st.latex(r"""
 + 2.32 \log_{10}(M_R) - 8.07
 """)
 
+# Option to choose which variable to solve for
+solve_for = st.radio("Select what to solve for:", ('SN (Structural Number)', 'W18 (Cumulative 18-kips ESAL)'))
+
 # Given constants input
 st.write("#### Parameters")
 
 with st.container(border=True):
     col1a, col1b, col1c = st.columns(3, gap='small')
 
-    with col1a:
-        W18 = st.number_input('W18 (cumulative)', value=None, placeholder='18-Kips ESAL')
+    if solve_for == 'SN (Structural Number)':
+        with col1a:
+            W18 = st.number_input('W18 (cumulative)', value=None, placeholder='18-Kips ESAL')
+
+    else:
+        with col1a:
+            SN = st.number_input('SN (Structural Number)', value=None)
 
     with col1b:
         Z_R = st.number_input('Zr', value=None, format='%0.4f')
@@ -36,30 +44,61 @@ with st.container(border=True):
     with col2b:
         M_R = st.number_input("Resilient Modulus (MR)", value=None, placeholder='psi')
 
-# Check if all inputs are provided before calculation
-if all(x is not None for x in [W18, Z_R, S_o, delta_PSI, M_R]):
-    try:
-        # Define variable
-        SN = sp.Symbol('SN', positive=True, real=True)
+if solve_for == 'SN (Structural Number)':
 
-        # Define the AASHTO Flexible Pavement equation
-        equation = -(sp.log(W18, 10)) + (Z_R * S_o + 9.36 * sp.log(SN + 1, 10) - 0.20 +
-                                         sp.log(delta_PSI / 2.7, 10) /
-                                         (0.40 + (1094 / (SN + 1)**5.19)) +
-                                         2.32 * sp.log(M_R, 10) - 8.07)
+    # Check if all inputs are provided before calculation
+    if all(x is not None for x in [W18, Z_R, S_o, delta_PSI, M_R]):
+        try:
+            # Define variable
+            SN = sp.Symbol('SN', positive=True, real=True)
 
-        # Use nsolve to solve the equation with an initial guess
-        SN_solution = sp.nsolve(equation, SN, 5)  # Initial guess of SN is set to 5
+            # Define the AASHTO Flexible Pavement equation
+            equation = -(sp.log(W18, 10)) + (Z_R * S_o + 9.36 * sp.log(SN + 1, 10) - 0.20 +
+                                             sp.log(delta_PSI / 2.7, 10) /
+                                             (0.40 + (1094 / (SN + 1)**5.19)) +
+                                             2.32 * sp.log(M_R, 10) - 8.07)
 
-        # Display result
-        st.write("#### Result")
-        st.success(f'Solution: SN = {SN_solution:.4f}')
+            # Use nsolve to solve the equation with an initial guess
+            SN_solution = sp.nsolve(equation, SN, 5)  # Initial guess of SN is set to 5
 
-    except ValueError as e:
-        st.error(f"Error in solving the equation: {e}")
-    except sp.SympifyError:
-        st.error("SympifyError: Unable to parse equation or input values.")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
+            # Display result
+            st.write("#### Result")
+            st.success(f'Solution: SN = {SN_solution:.4f}')
+
+        except ValueError as e:
+            st.error(f"Error in solving the equation: {e}")
+        except sp.SympifyError:
+            st.error("SympifyError: Unable to parse equation or input values.")
+        except Exception as e:
+            st.error(f"An unexpected error occurred: {e}")
+    else:
+        st.warning("Please fill in all the inputs to calculate SN.")
+
 else:
-    st.warning("Please fill in all the inputs to calculate SN.")
+    # Check if all inputs are provided before calculation
+    if all(x is not None for x in [SN, Z_R, S_o, delta_PSI, M_R]):
+        try:
+            # Define variable
+            W18 = sp.Symbol('W18', positive=True, real=True)
+
+            # Define the AASHTO Flexible Pavement equation
+            equation = -(sp.log(W18, 10)) + (Z_R * S_o + 9.36 * sp.log(SN + 1, 10) - 0.20 +
+                                             sp.log(delta_PSI / 2.7, 10) /
+                                             (0.40 + (1094 / (SN + 1) ** 5.19)) +
+                                             2.32 * sp.log(M_R, 10) - 8.07)
+
+            # Use nsolve to solve the equation with an initial guess
+            W18_solution = sp.nsolve(equation, W18, 10000)  # Initial guess of SN is set to 5
+
+            # Display result
+            st.write("#### Result")
+            st.success(f'Solution: W18 = {round(W18_solution, 3)} 18-kips ESAL')
+
+        except ValueError as e:
+            st.error(f"Error in solving the equation: {e}")
+        except sp.SympifyError:
+            st.error("SympifyError: Unable to parse equation or input values.")
+        except Exception as e:
+            st.error(f"An unexpected error occurred: {e}")
+    else:
+        st.warning("Please fill in all the inputs to calculate  W18.")
